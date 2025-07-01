4077dfe727f422130e28546b336ec072bb25060a
sakthi328 <sakthisankar328@gmail.com>
2025-07-01 09:00:02 +0530

    configured db

---
+++ modules/userModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    phone_number: Number,
+    password: String,
+})
+module.exports = mongoose.model("User", userSchema)
