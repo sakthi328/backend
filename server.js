9bc572d5305b5059921bcf9a190a9ae4bc160785
sakthi328 <sakthisankar328@gmail.com>
2025-07-10 19:26:16 +0530

    cors updated

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const userRoutes = require("./routes/userRoutes");
 const foodRoutes = require("./routes/foodRoutes");
 const connectDB = require("./config/db");
 const dontenv = require('dotenv');
+const cors = require('cors')
 
 dontenv.config();
 
 const app = express();
 connectDB();
 app.use(express.json());
-
+app.use(cors({
+  origin: process.env.FRONTEND_URL ||'http://localhost:5000'}));
 app.use('/api/users/',userRoutes)
 app.use('/api/foods/',foodRoutes)
 
