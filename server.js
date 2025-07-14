2dce068f3b4ca0fad9c3f19dd81358d847f212c8
sakthi328 <sakthisankar328@gmail.com>
2025-07-14 17:56:18 +0530

    cors update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 connectDB();
 app.use(express.json());
 app.use(cors({
-  origin: process.env.FRONTEND_URL ||'http://localhost:5000'}));
+  origin: process.env.FRONTEND_URL ||'http://localhost:5000',}));
 app.use('/api/users/',userRoutes)
 app.use('/api/foods/',foodRoutes)
 
